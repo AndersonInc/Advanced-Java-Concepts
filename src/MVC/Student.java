2216861b867b7adeeabb94db444e6cb3ef346dec
AndersonInc <andersonixon12@gmail.com>
2023-05-25 13:13:28 +0300

    Concepts

---
+++ src/MVC/Student.java
@@ -0,0 +1,19 @@
+package MVC;
+
+public class Student {
+    private String name;
+    private String rollno;
+
+    public void setName(String name){
+        this.name = name;
+    }
+    public String getName(){
+        return this.name;
+    }
+    public void setRollno(String rollno){
+        this.rollno = rollno;
+    }
+    public String getRollno(){
+        return this.rollno;
+    }
+}
